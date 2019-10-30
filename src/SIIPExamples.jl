40218b7208926c883b1c031f7a207f6a9268db94
Barrows <clayton.barrows@nrel.gov>
2019-10-30 13:16:47 -0600

    fixing module

--- src/SIIPExamples.jl
+++ src/SIIPExamples.jl
@@ -2,7 +2,7 @@ module SIIPExamples
 
 using Weave
 
-repo_directory = joinpath(@__DIR__,"..")
+repo_directory = dirname(joinpath(@__DIR__))
 
 """
 `weave_file(folder::AbstractString, file::AbstractString)`
@@ -11,30 +11,30 @@ Checks if the file has been modified since the last Weave and updates the notebo
 
 * `folder` = Name of the folder the tutorial is in
 * `file` = Name of the tutorial file
+* `force` = foce weave irrespective of file changes
 """
-function weave_file(folder,file)
-    cd(joinpath(repo_directory,"script",folder))
+function weave_file(folder, file; force = false)
     
     filename = split(file, ".")[1]
     srcpath = joinpath(repo_directory, "script", folder, file)
     testpath = joinpath(repo_directory, "test", folder, file)
     notebookpath = joinpath(repo_directory, "notebook", folder)
+    notebookfilepath = joinpath(notebookpath, join([filename,".ipynb"]))
 
-    if mtime(srcpath) > mtime(testpath) || mtime(testpath)==0
+    if mtime(srcpath) > mtime(testpath) || mtime(testpath)==0.0 || force
         @warn "Updating tests for $filename as it has been updated since the last weave."
         tangle(srcpath, out_path=testpath)
+        
     else
         @warn "Skipping tests for $filename as it has not been updated."
     end
 
-    if mtime(srcpath) > mtime(notebookpath) || mtime(notebookpath)==0
+    if mtime(srcpath) > mtime(notebookfilepath) || mtime(notebookfilepath)==0.0 || force
         @warn "Weaving $filename to Jupyter Notebook as it has been updated since the last weave."
-        notebook(srcpath, out_path = notebookpath, timeout = -1, nbconvert_options = "--allow-errors", jupyter_path = "jupyter")
+        convert_doc(srcpath, notebookfilepath)
     else
         @warn "Skipping Jupyter Notebook for $filename as it has not been updated."
     end
-
-    cd(joinpath(repo_directory,"src"))
 end
 
 """
@@ -44,13 +44,14 @@ Checks the files present in the specified folder for modifications and updates t
 
 * `folder` = Name of the folder to check
 """
-function weave_folder(folder)
+function weave_folder(folder; force=false)
     for file in readdir(joinpath(repo_directory,"script",folder))
         println("")
         println("Building $(joinpath(folder,file))")
         try
-            weave_file(folder,file)
+            weave_file(folder, file, force = force)
         catch
+            @error "failed to build $(joinpath(folder,file))"
         end
         println("")
     end
@@ -61,60 +62,9 @@ end
 
 Checks every tutorial for modifications and updates the notebook and tests accordingly.
 """
-function weave_all()
+function weave_all(;force=false)
     for folder in readdir(joinpath(repo_directory,"script"))
-        weave_folder(folder)
-    end
-end
-
-"""
-`weave_file_f(folder::AbstractString, file::AbstractString)`
-
-Use Weave to convert the given file irrespective of whether it has been modified or not.
-
-* `folder` = Name of the folder the tutorial is in
-* `file` = Name of the tutorial file
-"""
-function weave_file_f(folder,file)
-    cd(joinpath(repo_directory,"script",folder))
-    
-    srcpath = joinpath(repo_directory, "script", folder, file)
-    testpath = joinpath(repo_directory, "test", folder, file)
-    notebookpath = joinpath(repo_directory, "notebook", folder)
-
-    tangle(srcpath, out_path = testpath)
-    notebook(srcpath, notebookpath, -1, "--allow-errors")
-
-    cd(joinpath(repo_directory,"src"))
-end
-
-"""
-`weave_folder_f(folder::AbstractString, file::AbstractString)`
-
-Use Weave to convert every file in the specified folder irrespective of whether it has been modified or not.
-
-* `folder` = Name of the folder the tutorial is in
-"""
-function weave_folder_f(folder)
-    for file in readdir(joinpath(repo_directory,"script",folder))
-        println("")
-        println("Building $(joinpath(folder,file))")
-        try
-            weave_file_f(folder,file)
-        catch
-        end
-        println("")
-    end
-end
-
-"""
-`weave_all_f()`
-
-Use Weave to convert every tutorial irrespective of whether it has been modified or not.
-"""
-function weave_all_f()
-    for folder in readdir(joinpath(repo_directory,"script"))
-        weave_folder_f(folder)
+        weave_folder(folder; force = force)
     end
 end
 
