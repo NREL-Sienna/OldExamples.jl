a16186f33018ea1f55cc366374214550321f693e
Barrows <clayton.barrows@nrel.gov>
2020-03-11 11:02:58 -0600

    adding indexing by component example to matrices

--- script/PowerSystems_examples/network_matrices.jl
+++ script/PowerSystems_examples/network_matrices.jl
@@ -28,9 +28,15 @@ lodf = LODF(sys)
 # ### Indexing
 # Note that the axes of these matrices that correspond to buses are indexed by bus number
 # (::Int64) while the branch axes are indexed by branch name (::String). You can access
-# specific elements of the matrices as follows.
+# specific elements of the matrices as follows:
 
 ptdf["5",3]
+
+# Additionally, PowerSystems provides accessors to the network matrices that take `Componets`
+# as arguments so that you can pass references to the components themselves rather than the
+# name or number. For example:
+buses = collect(get_components(Bus,sys))
+ybus[buses[1],buses[2]]
 
 # If you would instead like to index by bus name, something like the following should work:
 busname2num = get_components(Bus,sys) |> (c -> Dict(zip(get_name.(c), get_number.(c))))
--- test/PowerSystems_examples/network_matrices.jl
+++ test/PowerSystems_examples/network_matrices.jl
@@ -10,6 +10,9 @@ lodf = LODF(sys)
 
 ptdf["5",3]
 
+buses = collect(get_components(Bus,sys))
+ybus[buses[1],buses[2]]
+
 busname2num = get_components(Bus,sys) |> (c -> Dict(zip(get_name.(c), get_number.(c))))
 ptdf["5", busname2num["bus3"]]
 
