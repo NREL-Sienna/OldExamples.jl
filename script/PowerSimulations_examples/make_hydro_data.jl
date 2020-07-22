ef75c1f637915e05787c8296936992b5bc7d8d48
Barrows <clayton.barrows@nrel.gov>
2020-07-22 08:59:23 -0600

    cleaning hydro creation

--- script/PowerSimulations_examples/make_hydro_data.jl
+++ script/PowerSimulations_examples/make_hydro_data.jl
@@ -1,7 +1,5 @@
 # This script is intended to be a dependency for the hydropower example.
 using PowerSystems
-using PowerSimulations
-const PSI = PowerSimulations
 using Statistics
 using Dates
 using TimeSeries
@@ -75,81 +73,3 @@ for gen in get_components(HydroEnergyReservoir, c_sys5_hy_uc)
         )
     end
 end
-
-#=
-load_timeseries_WK = [
-    [
-        TimeArray(
-            MultiDay,
-            repeat([mean(loadbus2_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-        ),
-        TimeArray(
-            MultiDay,
-            repeat([mean(loadbus3_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-        ),
-        TimeArray(
-            MultiDay,
-            repeat([mean(loadbus4_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-        ),
-    ],
-    [
-        TimeArray(
-            MultiDay + Day(n),
-            repeat([mean(loadbus2_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-        ),
-        TimeArray(
-            MultiDay + Day(n),
-            repeat([mean(loadbus3_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-        ),
-        TimeArray(
-            MultiDay + Day(n),
-            repeat([mean(loadbus4_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-        ),
-    ],
-]
-
-hydro_dispatch_timeseries_WK = [
-    [TimeArray(MultiDay, repeat([mean(wind_ts_DA[1:24])], inner = n) + rand(n) * 0.1)],
-    [TimeArray(
-        MultiDay + Day(n),
-        repeat([mean(wind_ts_DA[1:24])], inner = n) + rand(n) * 0.1,
-    )],
-]
-
-c_sys5_hy_wk = TEST_SYSTEMS["c_sys5_hy_uc"].build()
-clear_forecasts!(c_sys5_hy_wk)
-
-for t in 1:1
-    for (ix, l) in enumerate(get_components(PowerLoad, c_sys5_hy_wk))
-        add_forecast!(
-            c_sys5_hy_wk,
-            l,
-            Deterministic("get_maxactivepower", load_timeseries_WK[t][ix]),
-        )
-    end
-    for h in get_components(HydroEnergyReservoir, c_sys5_hy_wk)
-        add_forecast!(
-            c_sys5_hy_wk,
-            h,
-            Deterministic("get_rating", hydro_dispatch_timeseries_WK[t][1]),
-        )
-    end
-    for h in get_components(HydroEnergyReservoir, c_sys5_hy_wk)
-        add_forecast!(
-            c_sys5_hy_wk,
-            h,
-            Deterministic(
-                "get_storage_capacity",
-                2.0 .* hydro_dispatch_timeseries_WK[t][1],
-            ),
-        )
-    end
-    for h in get_components(HydroEnergyReservoir, c_sys5_hy_wk)
-        add_forecast!(
-            c_sys5_hy_wk,
-            h,
-            Deterministic("get_inflow", hydro_dispatch_timeseries_WK[t][1] .* 100.0),
-        )
-    end
-end
-=#
