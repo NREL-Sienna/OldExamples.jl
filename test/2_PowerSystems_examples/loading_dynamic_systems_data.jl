13e9a0785b0c6d2ff537b4b9234eff7003f01e20
Barrows <clayton.barrows@nrel.gov>
2020-11-19 22:12:38 -0700

    adding reradme and literate outputs

---
+++ test/2_PowerSystems_examples/loading_dynamic_systems_data.jl
@@ -0,0 +1,181 @@
+using SIIPExamples
+using PowerSystems
+const PSY = PowerSystems
+
+file_dir = joinpath(
+    dirname(dirname(pathof(SIIPExamples))),
+    "script",
+    "4_PowerSimulationsDynamics_examples",
+    "Data",
+)
+omib_sys = System(joinpath(file_dir, "OMIB.raw"))
+
+slack_bus = [b for b in get_components(Bus, omib_sys) if b.bustype == BusTypes.REF][1]
+inf_source = Source(
+    name = "InfBus", #name
+    available = true, #availability
+    active_power = 0.0,
+    reactive_power = 0.0,
+    bus = slack_bus, #bus
+    R_th = 0.0, #Rth
+    X_th = 5e-6, #Xth
+)
+add_component!(omib_sys, inf_source)
+
+get_components(Source, omib_sys)
+
+get_components(Generator, omib_sys)
+
+machine_classic() = BaseMachine(
+    0.0, #R
+    0.2995, #Xd_p
+    0.7087, #eq_p
+)
+
+shaft_damping() = SingleMass(
+    3.148, #H
+    2.0, #D
+)
+
+avr_none() = AVRFixed(0.0)
+
+tg_none() = TGFixed(1.0) #efficiency
+
+pss_none() = PSSFixed(0.0)
+
+static_gen = get_component(Generator, omib_sys, "generator-102-1")
+
+dyn_gen = DynamicGenerator(
+    name = get_name(static_gen),
+    ω_ref = 1.0,
+    machine = machine_classic(),
+    shaft = shaft_damping(),
+    avr = avr_none(),
+    prime_mover = tg_none(),
+    pss = pss_none(),
+)
+
+add_component!(omib_sys, dyn_gen, static_gen)
+
+to_json(omib_sys, joinpath(file_dir, "omib_sys.json"), force = true)
+
+sys_file_dir = joinpath(file_dir, "ThreeBusInverter.raw")
+threebus_sys = System(sys_file_dir)
+slack_bus = [b for b in get_components(Bus, threebus_sys) if b.bustype == BusTypes.REF][1]
+inf_source = Source(
+    name = "InfBus", #name
+    available = true, #availability
+    active_power = 0.0,
+    reactive_power = 0.0,
+    bus = slack_bus, #bus
+    R_th = 0.0, #Rth
+    X_th = 5e-6, #Xth
+)
+add_component!(threebus_sys, inf_source)
+
+#Define converter as an AverageConverter
+converter_high_power() = AverageConverter(rated_voltage = 138.0, rated_current = 100.0)
+
+#Define Outer Control as a composition of Virtual Inertia + Reactive Power Droop
+outer_control() = OuterControl(
+    VirtualInertia(Ta = 2.0, kd = 400.0, kω = 20.0),
+    ReactivePowerDroop(kq = 0.2, ωf = 1000.0),
+)
+
+#Define an Inner Control as a Voltage+Current Controler with Virtual Impedance:
+inner_control() = CurrentControl(
+    kpv = 0.59,     #Voltage controller proportional gain
+    kiv = 736.0,    #Voltage controller integral gain
+    kffv = 0.0,     #Binary variable enabling the voltage feed-forward in output of current controllers
+    rv = 0.0,       #Virtual resistance in pu
+    lv = 0.2,       #Virtual inductance in pu
+    kpc = 1.27,     #Current controller proportional gain
+    kic = 14.3,     #Current controller integral gain
+    kffi = 0.0,     #Binary variable enabling the current feed-forward in output of current controllers
+    ωad = 50.0,     #Active damping low pass filter cut-off frequency
+    kad = 0.2,      #Active damping gain
+)
+
+#Define DC Source as a FixedSource:
+dc_source_lv() = FixedDCSource(voltage = 600.0)
+
+#Define a Frequency Estimator as a PLL based on Vikram Kaura and Vladimir Blaskoc 1997 paper:
+pll() = KauraPLL(
+    ω_lp = 500.0, #Cut-off frequency for LowPass filter of PLL filter.
+    kp_pll = 0.084,  #PLL proportional gain
+    ki_pll = 4.69,   #PLL integral gain
+)
+
+#Define an LCL filter:
+filt() = LCLFilter(lf = 0.08, rf = 0.003, cf = 0.074, lg = 0.2, rg = 0.01)
+
+machine_oneDoneQ() = OneDOneQMachine(
+    0.0, #R
+    1.3125, #Xd
+    1.2578, #Xq
+    0.1813, #Xd_p
+    0.25, #Xq_p
+    5.89, #Td0_p
+    0.6, #Tq0_p
+)
+
+shaft_no_damping() = SingleMass(
+    3.01, #H (M = 6.02 -> H = M/2)
+    0.0, #D
+)
+
+avr_type1() = AVRTypeI(
+    20.0, #Ka - Gain
+    0.01, #Ke
+    0.063, #Kf
+    0.2, #Ta
+    0.314, #Te
+    0.35, #Tf
+    0.001, #Tr
+    (min = -5.0, max = 5.0),
+    0.0039, #Ae - 1st ceiling coefficient
+    1.555, #Be - 2nd ceiling coefficient
+)
+
+#No TG
+tg_none() = TGFixed(1.0) #efficiency
+
+#No PSS
+pss_none() = PSSFixed(0.0) #Vs
+
+for g in get_components(Generator, threebus_sys)
+    #Find the generator at bus 102
+    if get_number(get_bus(g)) == 102
+        #Create the dynamic generator
+        case_gen = DynamicGenerator(
+            get_name(g),
+            1.0, # ω_ref,
+            machine_oneDoneQ(), #machine
+            shaft_no_damping(), #shaft
+            avr_type1(), #avr
+            tg_none(), #tg
+            pss_none(), #pss
+        )
+        #Attach the dynamic generator to the system by specifying the dynamic and static part
+        add_component!(threebus_sys, case_gen, g)
+        #Find the generator at bus 103
+    elseif get_number(get_bus(g)) == 103
+        #Create the dynamic inverter
+        case_inv = DynamicInverter(
+            get_name(g),
+            1.0, # ω_ref,
+            converter_high_power(), #converter
+            outer_control(), #outer control
+            inner_control(), #inner control voltage source
+            dc_source_lv(), #dc source
+            pll(), #pll
+            filt(), #filter
+        )
+        #Attach the dynamic inverter to the system
+        add_component!(threebus_sys, case_inv, g)
+    end
+end
+
+to_json(threebus_sys, joinpath(file_dir, "threebus_sys.json"), force = true)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
--- test/4_PowerSimulationsDynamics_examples/02_line_dynamics.jl
+++ test/4_PowerSimulationsDynamics_examples/02_line_dynamics.jl
@@ -1,33 +1,9 @@
-# Line Modeling simulation with [PowerSimulationsDynamics.jl](https://github.com/NREL-SIIP/PowerSimulationsDynamics.jl)
-
-# **Originally Contributed by**: Rodrigo Henriquez and José Daniel Lara
-
-# ## Introduction
-
-# This tutorial will introduce an example of considering dynamic lines in `PowerSimulationsDynamics`.
-# Note that this tutorial is for `PowerSimulationsDynamics`.
-
-# This tutorial presents a simulation of a three-bus system, with an infinite bus (represented as a
-# voltage source behind an impedance) at bus 1, a one d- one q- machine on bus 2 and an inverter
-# of 19 states, as a virtual synchronous machine at bus 3. The perturbation will be the trip of
-# two of the three circuits (triplicating its resistance and impedance) of the line that connects
-# bus 1 and bus 3. This case also consider a dynamic line model for connection between buses
-# 2 and 3. We will compare it against a system without dynamic lines.
-
-# In addition, note that `PowerSimulationsDynamics` will convert ConstantPower loads to RLC
-# loads for transient simulations.
-
-# It is recommended to check `Tutorial 1: OMIB` first, since that includes more details and
-# explanations on all definitions and functions.
-
-# # Step 1: Package Initialization
 using SIIPExamples
 using PowerSimulationsDynamics
 using PowerSystems
 using Sundials
 using Plots
 
-# # Step 2: Data creation
 file_dir = joinpath(
     dirname(dirname(pathof(SIIPExamples))),
     "script",
@@ -36,14 +12,7 @@ file_dir = joinpath(
 )
 threebus_sys = System(joinpath(file_dir, "threebus_sys.json"));
 
-# In addition, we will create a new copy of the system on which we will simulate the same
-# case, but will consider dynamic lines:
-
 threebus_sys_dyn = deepcopy(threebus_sys);
-
-# # Step 3: Create the fault and simulation on the Static Lines system
-
-# First, we construct the perturbation, by properly computing the new Ybus on the system:
 
 #Make a copy of the original system
 sys2 = deepcopy(threebus_sys)
@@ -65,8 +34,6 @@ Ybus_change = NetworkSwitch(
     Ybus_fault, #New YBus
 );
 
-# Now, we construct the simulation:
-
 #Time span of our simulation
 tspan = (0.0, 30.0)
 
@@ -78,14 +45,10 @@ sim = Simulation(
     Ybus_change, #Type of perturbation
 )
 
-# We can obtain the initial conditions as:
-
 #Will print the initial states. It also give the symbols used to describe those states.
 print_device_states(sim)
 #Will export a dictionary with the initial condition values to explore
 x0_init = get_initial_conditions(sim)
-
-# # Step 4: Run the simulation of the Static Lines System
 
 #Run the simulation
 execute!(
@@ -94,25 +57,15 @@ execute!(
     dtmax = 0.02, #Maximum step size
 )
 
-# # Step 5: Store the solution
-
 series2 = get_voltagemag_series(sim, 102)
 zoom = [
     (series2[1][ix], series2[2][ix])
     for (ix, s) in enumerate(series2[1]) if (s > 0.90 && s < 1.6)
 ];
 
-# # Step 3.1: Create the fault and simulation on the Dynamic Lines system
-
-# An important aspect to consider is that DynamicLines must not be considered in the computation of the Ybus. First we construct the Dynamic Line, by finding the Line named "BUS 2-BUS 3-i_3", and then adding it to the system.
-
-# get component return the Branch on threebus_sys_dyn named "BUS 2-BUS 3-i_3"
 dyn_branch = DynamicBranch(get_component(Branch, threebus_sys_dyn, "BUS 2-BUS 3-i_3"))
-# Adding a dynamic line will inmediately remove the static line from the system.
-add_component!(threebus_sys_dyn, dyn_branch)
 
-# Similarly, we construct the Ybus fault by creating a copy of the original system, but
-# removing the Line "BUS 2-BUS 3-i_3" to avoid considering it in the Ybus:
+add_component!(threebus_sys_dyn, dyn_branch)
 
 #Make a copy of the original system
 sys3 = deepcopy(threebus_sys);
@@ -136,14 +89,8 @@ Ybus_change_dyn = PowerSimulationsDynamics.NetworkSwitch(
     Ybus_fault_dyn, #New YBus
 )
 
-# # Step 4.1: Run the simulation of the Dynamic Lines System
-
-# Now, we construct the simulation:
-
-# Time span of our simulation
 tspan = (0.0, 30.0)
 
-# Define Simulation
 sim_dyn = Simulation(
     pwd(), #folder to output results
     threebus_sys_dyn, #system
@@ -151,21 +98,16 @@ sim_dyn = Simulation(
     Ybus_change_dyn, #Type of perturbation
 )
 
-# Run the simulation
 execute!(
     sim_dyn, #simulation structure
     IDA(), #Sundials DAE Solver
     dtmax = 0.02, #Maximum step size
 )
 
-# We can obtain the initial conditions as:
-
 #Will print the initial states. It also give the symbols used to describe those states.
 print_device_states(sim_dyn)
 #Will export a dictionary with the initial condition values to explore
 x0_init_dyn = get_initial_conditions(sim_dyn)
-
-# # Step 5.1: Store the solution
 
 series2_dyn = get_voltagemag_series(sim_dyn, 102)
 zoom_dyn = [
@@ -173,14 +115,11 @@ zoom_dyn = [
     for (ix, s) in enumerate(series2_dyn[1]) if (s > 0.90 && s < 1.6)
 ];
 
-# # Step 6.1: Compare the solutions:
-
-# We can observe the effect of Dynamic Lines
-
 Plots.plot(series2_dyn, label = "V_gen_dyn")
 Plots.plot!(series2, label = "V_gen_st", xlabel = "Time [s]", ylabel = "Voltage [pu]")
 
-# that looks quite similar. The differences can be observed in the zoom plot:
-
 Plots.plot(zoom_dyn, label = "V_gen_dyn")
 Plots.plot!(zoom, label = "V_gen_st", xlabel = "Time [s]", ylabel = "Voltage [pu]")
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
