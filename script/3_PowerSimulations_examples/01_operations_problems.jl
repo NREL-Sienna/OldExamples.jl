774b82865b72ed664c788246e09e6971e4b50be4
cbarrows <clayton.barrows@nrel.gov>
2021-12-03 05:35:07 -0700

    single step problem working

--- script/3_PowerSimulations_examples/01_operations_problems.jl
+++ script/3_PowerSimulations_examples/01_operations_problems.jl
@@ -9,7 +9,7 @@
 # scheduling problems (Operations Problems). Operations problems form the fundamental
 # building blocks for [sequential simulations](https://nbviewer.jupyter.org/github/NREL-SIIP/SIIPExamples.jl/blob/master/notebook/3_PowerSimulations_examples/02_sequential_simulations.ipynb).
 # This example shows how to specify and customize a the mathematics that will be applied to the data with
-# an `OperationsProblemTemplate`, build and execute an `OperationsProblem`, and access the results.
+# an `ProblemTemplate`, build and execute an `DecisionModel`, and access the results.
 
 # ## Dependencies
 using SIIPExamples
@@ -33,9 +33,9 @@ using Cbc #solver
 
 sys = build_system(PSITestSystems, "modified_RTS_GMLC_DA_sys")
 
-# ## Define a problem specification with an `OperationsProblemTemplate`
+# ## Define a problem specification with an `ProblemTemplate`
 # You can create an empty template with:
-template_uc = OperationsProblemTemplate()
+template_uc = ProblemTemplate()
 
 # Now, you can add a `DeviceModel` for each device type to create an assignment between PowerSystems device types
 # and the subtypes of `AbstractDeviceFormulation`. PowerSimulations has a variety of different
@@ -79,50 +79,50 @@ set_service_model!(template_uc, VariableReserve{ReserveDown}, RangeReserve)
 # choose a copper plate formulation. But there are dozens of specifications available through
 # an integration with [PowerModels.jl](https://github.com/lanl-ansi/powermodels.jl). *Note that
 # many formulations will require appropriate data and may be computationally intractable*
-set_transmission_model!(template_uc, CopperPlatePowerModel)
+set_network_model!(template_uc, NetworkModel(CopperPlatePowerModel))
 
-# ## `OperationsProblem`
-# Now that we have a `System` and an `OperationsProblemTemplate`, we can put the two together
-# to create an `OperationsProblem` that we solve.
+# ## `DecisionModel`
+# Now that we have a `System` and an `ProblemTemplate`, we can put the two together
+# to create an `DecisionModel` that we solve.
 
 # ### Optimizer
 # It's most convenient to define an optimizer instance upfront and pass it into the
-# `OperationsProblem` constructor. For this example, we can use the free Cbc solver with a
+# `DecisionModel` constructor. For this example, we can use the free Cbc solver with a
 # relatively relaxed MIP gap (`ratioGap`) setting to improve speed.
 
 solver = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 1, "ratioGap" => 0.5)
 
-# ### Build an `OperationsProblem`
-# The construction of an `OperationsProblem` essentially applies an `OperationsProblemTemplate`
+# ### Build an `DecisionModel`
+# The construction of an `DecisionModel` essentially applies an `ProblemTemplate`
 # to `System` data to create a JuMP model.
 
-op_problem = OperationsProblem(template_uc, sys; optimizer = solver, horizon = 24)
+op_problem = DecisionModel(template_uc, sys; optimizer = solver, horizon = 24)
 
 build!(op_problem, output_dir = mktempdir())
 
-#nb # The principal component of the `OperationsProblem` is the JuMP model. For small problems,
+#nb # The principal component of the `DecisionModel` is the JuMP model. For small problems,
 #nb # you can inspect it by simply printing it to the screen:
 #nb # ```julia
-#nb # op_problem.internal.optimization_container.JuMPmodel
+#nb # PSI.get_jump_model(op_problem)
 #nb # ```
 #nb #
 #nb # For anything of reasonable size, that will be unmanageable. But you can print to a file:
 #nb # ```julia
-#nb # f = open("testmodel.txt","w"); print(f,op_problem.internal.optimization_container.JuMPmodel); close(f)
+#nb # f = open("testmodel.txt","w"); print(f,PSI.get_jump_model(op_problem)); close(f)
 #nb # ```
 #nb #
-#nb # In addition to the JuMP model, an `OperationsProblem` keeps track of a bunch of metadata
+#nb # In addition to the JuMP model, an `DecisionModel` keeps track of a bunch of metadata
 #nb # about the problem and some references to pretty names for constraints and variables.
 #nb # All of these details are contained within the `optimization_container` field.
 #
 #nb print_struct(typeof(op_problem.internal.optimization_container))
 #
-#nb # ### Solve an `OperationsProblem`
+#nb # ### Solve an `DecisionModel`
 #
 #nb solve!(op_problem)
 #
 #nb # ## Results Inspection
-#nb # PowerSimulations collects the `OperationsProblem` results into a struct:
+#nb # PowerSimulations collects the `DecisionModel` results into a struct:
 #
 #nb print_struct(PSI.ProblemResults)
 #
@@ -140,7 +140,7 @@ build!(op_problem, output_dir = mktempdir())
 #nb # ### Variable Values
 #nb # The solution value data frames for variables can be accessed by:
 #
-#nb variable_values = get_variables(res)
+#nb variable_values = get_variable_values(res)
 #
 # ## Plotting
 # Take a look at the examples in [the plotting folder.](../../notebook/3_PowerSimulations_examples/Plotting)
